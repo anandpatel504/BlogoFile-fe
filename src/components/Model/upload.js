3ab734e475525050d52e718ef7aa8f11c4ea7d6c
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2022-04-30 00:27:10 +0530

    upload changes

--- src/components/Model/upload.js
+++ src/components/Model/upload.js
@@ -28,17 +28,17 @@ export default function Dropzone({ onFileAccepted }) {
     (acceptedFiles) => {
       // onFileAccepted(acceptedFiles[0]);
       console.log(acceptedFiles);
-      setImage(acceptedFiles[0])
+      setImage(acceptedFiles[0]);
     },
     // [onFileAccepted]
     []
   );
 
-  const onSubmitForm = ()=>{
+  const onSubmitForm = () => {
     const user = reactLocalStorage.getObject("user");
-    const data = new FormData() 
-    data.append('myimage', image)
-    if (!image){
+    const data = new FormData();
+    data.append("myimage", image);
+    if (!image) {
       return toast({
         title: "Select image to upload",
         status: "error",
@@ -48,40 +48,41 @@ export default function Dropzone({ onFileAccepted }) {
         position: "bottom-right",
       });
     }
-    setLoading(true)
+    setLoading(true);
     axios
-    .post(
-      process.env.REACT_APP_BACKEND_API_URL +
-        "/uploadPhoto" +
-        "?token=" +
-        user.token,  data
-    )
-    .then((res) => {
-      if (res.data.status == "success") {
-        toast({
-          title: "Image uploaded on cloud",
-          status: "success",
-          variant: "left-accent",
-          duration: 9000,
-          isClosable: true,
-          position: "bottom-right",
-        });
-        window.location.reload()
-      } else {
-        toast({
-          title: res.data.message,
-          status: "error",
-          variant: "left-accent",
-          duration: 9000,
-          isClosable: true,
-          position: "bottom-right",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .post(
+        process.env.REACT_APP_BACKEND_API_URL +
+          "/uploadPhoto" +
+          "?token=" +
+          user.token,
+        data
+      )
+      .then((res) => {
+        if (res.data.status == "success") {
+          toast({
+            title: "Image uploaded on cloud",
+            status: "success",
+            variant: "left-accent",
+            duration: 9000,
+            isClosable: true,
+            position: "bottom-right",
+          });
+          window.location.reload();
+        } else {
+          toast({
+            title: res.data.message,
+            status: "error",
+            variant: "left-accent",
+            duration: 9000,
+            isClosable: true,
+            position: "bottom-right",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
@@ -106,10 +107,17 @@ export default function Dropzone({ onFileAccepted }) {
       <Button
         leftIcon={<MdUpload />}
         style={{ marginLeft: "auto" }}
+        display={{base: "none", md: "block"}}
         onClick={onOpen}
       >
         Upload Photo
       </Button>
+      <Button
+        display={{ base: "block", md: "none" }}
+        leftIcon={<MdUpload />}
+        style={{ marginLeft: "auto" }}
+        onClick={onOpen}
+      ></Button>
       <Modal
         isCentered
         onClose={onClose}
@@ -136,14 +144,28 @@ export default function Dropzone({ onFileAccepted }) {
             >
               <input {...getInputProps()} />
               <Icon as={AiFillFileAdd} mr={2} />
-              <p>{image? (image?.name+" | "+(image?.size/1000000).toFixed(3))+" MB" :dropText}</p>
+              <p>
+                {image
+                  ? image?.name +
+                    " | " +
+                    (image?.size / 1000000).toFixed(3) +
+                    " MB"
+                  : dropText}
+              </p>
             </Center>
           </ModalBody>
           <ModalFooter>
             <Button colorScheme="red" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button onClick={onSubmitForm} isLoading={isloading} loadingText="Uploading image." colorScheme="green">Upload Image</Button>
+            <Button
+              onClick={onSubmitForm}
+              isLoading={isloading}
+              loadingText="Uploading image."
+              colorScheme="green"
+            >
+              Upload Image
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
